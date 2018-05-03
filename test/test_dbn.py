9a97cc7cfbaea07358eb31e13875227941edfca7
Zny <zny08@qq.com>
2018-05-03 09:10:07 +0800

    调整代码

--- test/test_dbn.py
+++ test/test_dbn.py
@@ -1,4 +1,4 @@
-import ReadFile
+from ReadFile import ReadFile
 import preprocess
 import dbn_model
 import os
@@ -6,7 +6,7 @@ import os
 path_cur = os.path.abspath('.')
 path_pre = os.path.abspath('..')
 
-read = ReadFile.ReadFile(path_pre + "/NSL_KDD-master").get_data()
+read = ReadFile(path_pre + "/NSL_KDD-master").get_data()
 data_pp = preprocess.Preprocess(read).do_predict_preprocess()
 do_dbn = dbn_model.DBN(data_pp).do_dbn_with_weight_matrix(path_pre + "/save/weight_matrix")
 
